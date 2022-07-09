375825041adf7967e8b2a5f1b7358953c8da15d2
Austin <andrews.austin.94@gmail.com>
2022-07-09 17:25:08 +0000

    Add form, routes

--- redwoodblog/web/src/pages/ContactPage/ContactPage.js
+++ redwoodblog/web/src/pages/ContactPage/ContactPage.js
@@ -1,19 +1,36 @@
-import { Link, routes } from '@redwoodjs/router'
+
 import { MetaTags } from '@redwoodjs/web'
+import { 
+  FieldError,
+  Form,
+  TextField,
+  TextAreaField,
+  Submit
+} from '@redwoodjs/forms'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <h1>ContactPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ContactPage/ContactPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>contact</code>, link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
+      <Form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <TextField name="name" validation={{ required: true }} errorClassName="error" />
+        <FieldError name="name" className="error"/>
+
+        <label htmlFor="email">Email</label>
+        <TextField name="email" validation={{ required: true }} errorClassName="error" />
+        <FieldError name="email" className="error"/>
+
+        <label htmlFor="message">Message</label>
+        <TextAreaField name="message" validation={{ required: true }} errorClassName="error" />
+        <FieldError name="message" className="error" />
+
+        <Submit>Save</Submit>
+      </Form>
     </>
   )
 }
